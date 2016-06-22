cb6d902c0a1ef897dbfdb8a25cf399c9104b2bc6
mwongeraE <evansmwongera@gmail.com>
2016-06-22 17:41:51 +0300

    taskdbhelper correct

--- app/src/main/java/com/todolist/mwongera/todolist/db/TaskDbHelper.java
+++ app/src/main/java/com/todolist/mwongera/todolist/db/TaskDbHelper.java
@@ -9,14 +9,15 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class TaskDbHelper extends SQLiteOpenHelper {
 
-    public TaskDbHelper(Context context){
+
+    public TaskDbHelper(Context context) {
         super(context, TaskContract.DB_NAME, null, TaskContract.DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTable = "CREATE TABLE " + TaskContract.TaskEntry.TABLE + " ( " +
-                TaskContract.TaskEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                TaskContract.TaskEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 TaskContract.TaskEntry.COL_TASK_TITLE + " TEXT NOT NULL);";
 
         db.execSQL(createTable);
