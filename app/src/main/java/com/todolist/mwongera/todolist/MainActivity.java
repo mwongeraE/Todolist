d3946c5d8d1976cc10b8cf892df053a841f02d69
mwongeraE <evansmwongera@gmail.com>
2016-06-22 17:26:37 +0300

    main activity corrections

--- app/src/main/java/com/todolist/mwongera/todolist/MainActivity.java
+++ app/src/main/java/com/todolist/mwongera/todolist/MainActivity.java
@@ -35,86 +35,50 @@ public class MainActivity extends AppCompatActivity {
 
         mHelper = new TaskDbHelper(this);
         mTaskListView = (ListView) findViewById(R.id.list_todo);
-        SQLiteDatabase db = mHelper.getReadableDatabase();
-        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
-                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
-                null,null,null,null,null
-                );
-        while (cursor.moveToNext()) {
-            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
-            Log.d(TAG, "Task: " + cursor.getString(idx));
-        }
-        cursor.close();
-        db.close();
+
+        updateUI();
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-            getMenuInflater().inflate(R.menu.main_menu,menu);
-            return super.onCreateOptionsMenu(menu);
-        }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-            switch (item.getItemId()) {
-                case R.id.action_add_task:
-                    Log.d(TAG, "Add a new task");
-                    return true;
-
-                default:
-                    return super.onOptionsItemSelected(item);
-            }
-        }
-    final EditText taskEditText = new EditText(this);
-    AlertDialog dialog = new AlertDialog.Builder(this)
-            .setTitle("Add a new task")
-            .setMessage("What do you want to do next?")
-            .setView(taskEditText)
-            .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    String task = String.valueOf(taskEditText.getText());
-                    SQLiteDatabase db = mHelper.getWritableDatabase();
-                    ContentValues values = new ContentValues();
-                    values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
-                    db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
-                            null,
-                            values,
-                            SQLiteDatabase.CONFLICT_REPLACE);
-                    db.close();
-                }
-            })
-            .setNegativeButton("Cancel", null)
-            .create();
-    dialog.show;
-    return true;
-
-    default:
-        return super.onOptionsItemSelected(item);
-
-    private void updateUI() {
-        ArrayList<String> taskList = new ArrayList<>();
-        SQLiteDatabase db = mHelper.getReadableDatabase();
-        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
-                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
-                null, null, null, null, null);
-        while (cursor.moveToNext()) {
-            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
-            taskList.add(cursor.getString(idx));
-        }
-        if (mAdapter == null){
-            mAdapter = new ArrayAdapter<>(this,
-                    R.layout.item_todo,
-                    R.id.task_title,
-                    taskList);
-            mTaskListView.setAdapter(mAdapter);
-        }else {
-            mAdapter.clear();
-            mAdapter.addAll(taskList);
-            mAdapter.notifyDataSetChanged();
-        }
-        cursor.close();
-        db.close();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                SQLiteDatabase db = mHelper.getWritableDatabase();
+                                ContentValues values = new ContentValues();
+                                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                                        null,
+                                        values,
+                                        SQLiteDatabase.CONFLICT_REPLACE);
+                                db.close();
+                                updateUI();
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void deleteTask(View view) {
         View parent = (View) view.getParent();
         TextView taskTextView = (TextView) parent.findViewById(R.id.task_title);
@@ -126,4 +90,32 @@ public class MainActivity extends AppCompatActivity {
         db.close();
         updateUI();
     }
+
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            taskList.add(cursor.getString(idx));
+        }
+
+        if (mAdapter == null) {
+            mAdapter = new ArrayAdapter<>(this,
+                    R.layout.item_todo,
+                    R.id.task_title,
+                    taskList);
+            mTaskListView.setAdapter(mAdapter);
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        db.close();
+    }
+
 }
